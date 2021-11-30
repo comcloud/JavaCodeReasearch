07139b90f02453887829526a93876d0f3cbc44c6
zhangyulei02 <2230817302@qq.com>
2021-11-30 21:01:04 +0800

    验证多线程问题

--- src/main/java/com/cloud/test1/Test.java
+++ src/main/java/com/cloud/test1/Test.java
@@ -12,11 +12,35 @@ package com.cloud.test1;
  * @version :Test.java v1.0 2021/11/11 4:05 下午 zhangyulei Exp $
  */
 public class Test {
-    public static void main(String[] args) {
-        Student student = new Student("玛咖", "女", "清华");
-        CollegeStudent collegeStudent = new CollegeStudent("玛咖", "女", "清华", "建模");
-        student.study();
-        collegeStudent.study();
 
+    volatile int a = 1;
+    volatile int b = 1;
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        new Thread(test::add).start();
+        new Thread(test::compare).start();
+
+    }
+
+    public void add() {
+        System.out.println("add start");
+        for (int i = 0; i < 10000; i++) {
+            a++;
+            b++;
+        }
+        System.out.println("add done");
+    }
+
+    public void compare() {
+        System.out.println("compare start");
+        for (int i = 0; i < 10000; i++) {
+            //a始终等于b吗？
+            if (a < b) {
+                System.out.println("a:" + a + ",b:" + b + " " + (a > b));
+                //最后的a>b应该始终是false吗？
+            }
+        }
+        System.out.println("compare done");
     }
 }
